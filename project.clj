cf78717ee0d20aaa29872a319b5b0308c2f6bf93
Thomas Heller <info@zilence.net>
2015-10-24 14:47:40 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-devtools "0.1.24"
+(defproject thheller/shadow-devtools "0.1.27"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
