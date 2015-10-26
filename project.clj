f9025c755dc56e0661b9f3d14ec0844ce0180196
Thomas Heller <info@zilence.net>
2015-10-26 09:53:31 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-devtools "0.1.28"
+(defproject thheller/shadow-devtools "0.1.30"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,7 @@
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [thheller/shadow-client "1.0.1"]
-                 [thheller/shadow-build "1.0.141"]]
+                 [thheller/shadow-build "1.0.143"]]
 
   :source-paths ["src/clj"
                  "src/cljs"]
