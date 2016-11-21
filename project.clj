64c9a185451e9e5b710d60ceac97d54c7a52c351
Thomas Heller <th.heller@gmail.com>
2016-11-22 00:30:17 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,18 +1,19 @@
-(defproject thheller/shadow-devtools "0.1.59"
+(defproject thheller/shadow-devtools "0.1.60"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[com.cognitect/transit-cljs "0.8.239"]
-                 [aleph "0.4.1"]
-                 [org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [org.clojure/core.async "0.2.385"]
-                 [thheller/shadow-client "1.0.171"]
-                 [thheller/shadow-build "1.0.237"]
-                 [hiccup "1.0.5"]
-                 [ring/ring-core "1.5.0"]]
+  :dependencies
+  [[com.cognitect/transit-cljs "0.8.239"]
+   [aleph "0.4.2-alpha10"]
+   [org.clojure/clojure "1.9.0-alpha14"]
+   [org.clojure/clojurescript "1.9.293"]
+   [org.clojure/core.async "0.2.395"]
+   [thheller/shadow-client "1.0.175"]
+   [thheller/shadow-build "1.0.239"]
+   [hiccup "1.0.5"]
+   [ring/ring-core "1.5.0"]]
 
   :source-paths ["src/clj"
                  "src/cljs"]
