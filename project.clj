8925151363c17ec16d403e2d20706fcc44eda2e6
Thomas Heller <th.heller@gmail.com>
2017-05-19 15:32:33 +0200

    bump versions
    I should write a script for this

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "1.0.20170518"
+(defproject thheller/shadow-cljs "1.0.20170519"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
