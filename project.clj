7439960533269a72d9e547e19d89196a19b7ab29
Thomas Heller <th.heller@gmail.com>
2017-10-13 08:36:29 +0200

    content type header for push-state handler

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "2.0.11"
+(defproject thheller/shadow-cljs "2.0.12"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
--- src/main/shadow/http/push_state.clj
+++ src/main/shadow/http/push_state.clj
@@ -3,6 +3,10 @@
 
 (defn handle [{:keys [http-root devtools] :as req}]
   (let [index-name
-        (get devtools :push-state/index "index.html")]
+        (get devtools :push-state/index "index.html")
+
+        headers
+        (get devtools :push-state/headers {"content-type" "text/html; charset=utf-8"})]
     {:status 200
+     :headers headers
      :body (slurp (io/file http-root index-name))}))
