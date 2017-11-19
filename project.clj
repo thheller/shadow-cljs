d1b2c5edefe9c5147e8f8ca1f74addab6ef88808
Thomas Heller <th.heller@gmail.com>
2017-11-19 17:14:36 +0100

    webpack stats converter

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "2.0.91"
+(defproject thheller/shadow-cljs "2.0.92"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
--- src/repl/shadow/cljs/ui_test.clj
+++ src/repl/shadow/cljs/ui_test.clj
@@ -5,7 +5,10 @@
     [shadow.cljs.devtools.api :as api]
     [shadow.build.closure :as closure]
     [shadow.cljs.util :as util]
-    [shadow.build.data :as data]))
+    [shadow.build.data :as data]
+    [clojure.edn :as edn]
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]))
 
 (deftest test-bundle-info
 
@@ -13,3 +16,24 @@
         (api/generate-bundle-info :browser)]
 
     (pprint report)))
+
+
+(deftest test-convert-to-webpack-stats
+  (let [{:keys [build-sources] :as data}
+        (-> (slurp ".shadow-cljs/builds/browser/release/bundle-info.edn")
+            (edn/read-string))]
+
+    (->> {:assets []
+          :chunks []
+          :modules
+          (->> build-sources
+               (map-indexed
+                 (fn [idx {:keys [resource-name optimized-size js-size]}]
+                   {:id idx
+                    :identifier (str "/" resource-name)
+                    :name (str "/" resource-name)
+                    :size (or optimized-size js-size)
+                    :reasons []}))
+               (into []))}
+         (json/write-str )
+         (spit (io/file "tmp/stats.json")))))
