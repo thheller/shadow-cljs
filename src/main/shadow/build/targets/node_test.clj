f7a398d03a7e3fc06be2c7473498325bd932a680
Thomas Heller <th.heller@gmail.com>
2018-03-08 13:27:16 +0100

    add :autorun option for :node-test target
    runs the generated output file automatically with node on build flush

--- src/main/shadow/build/targets/node_test.clj
+++ src/main/shadow/build/targets/node_test.clj
@@ -6,12 +6,14 @@
             [shadow.build.classpath :as cp]
             [shadow.build.targets.node-script :as node-script]
             [shadow.cljs.util :as util]
+            [shadow.cljs.devtools.server.util :refer (pipe)]
             [hiccup.page :refer (html5)]
             [clojure.java.io :as io]
             [cljs.compiler :as cljs-comp]
             [shadow.build.api :as build-api]
             [shadow.build.output :as output]
-            [shadow.build.data :as data]))
+            [shadow.build.data :as data])
+  (:import [java.lang ProcessBuilder$Redirect]))
 
 (defn configure [{::build/keys [config mode] :as state}]
   (-> state
@@ -47,6 +49,27 @@
         ;; re-analyze modules since we modified the entries
         (modules/analyze))))
 
+(defn autorun-test [{::build/keys [config] :as state}]
+  (util/with-logged-time
+    [state {:type ::autorun}]
+    (let [script-args
+          ["node" (:output-to config)]
+
+          proc
+          (-> (ProcessBuilder. (into-array script-args))
+              (.directory nil)
+              (.start))]
+
+      (println "========= Running Tests =======================")
+
+      (.start (Thread. (bound-fn [] (pipe proc (.getInputStream proc) *out*))))
+      (.start (Thread. (bound-fn [] (pipe proc (.getErrorStream proc) *err*))))
+
+      ;; FIXME: what if this doesn't terminate?
+      (let [exit-code (.waitFor proc)]
+        (println "===============================================")
+        (assoc state ::exit-code exit-code)))))
+
 (defn process
   [{::build/keys [stage mode config] :as state}]
   (-> state
@@ -56,4 +79,9 @@
 
         (= stage :compile-prepare)
         (compile-prepare))
-      (node-script/process)))
+      (node-script/process)
+      (cond->
+        (and (= stage :flush)
+             (:autorun config))
+        (autorun-test)
+        )))
