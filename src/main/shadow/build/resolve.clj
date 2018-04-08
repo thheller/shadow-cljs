thheller/shadow-cljs

commit 0a3a66042b82ce6a1c98d6377792f749d81c9a7d

improve resolve perf when recompiling in watch

src/main/shadow/build/resolve.clj
@@ -215,27 +215,35 @@
   {:pre [(data/build-state? state)
          (string? require)]}
 
-  (let [{:keys [resource-id ns] :as rc}
-        (find-resource-for-string state require-from require)]
+  (let [resolved-sym (get-in state [:str->sym require-from-ns require])]
+    ;; in a watch recompile cycle strings may already be resolved so we can re-use those
+    ;; resolve is a bit costly since it has to do a bunch of FS interop
+    (if resolved-sym
+      (let [rc (data/get-source-by-provide state resolved-sym)]
+        (resolve-deps state rc))
 
-    (when-not rc
-      (throw (ex-info
-               (if require-from
-                 (format "The required JS dependency \"%s\" is not available, it was required by \"%s\"." require (:resource-name require-from))
-                 (format "The required JS dependency \"%s\" is not available." require))
-               {:tag ::missing-js
-                :require require
-                :require-from (:resource-name require-from)})))
+      ;; fresh resolve
+      (let [{:keys [resource-id ns] :as rc}
+            (find-resource-for-string state require-from require)]
 
-    (-> state
-        (data/maybe-add-source rc)
-        (cond->
-          require-from-ns
-          (data/add-string-lookup require-from-ns require ns)
-          (nil? require-from)
-          (update :js-entries conj ns))
-        (resolve-deps rc)
-        )))
+        (when-not rc
+          (throw (ex-info
+                   (if require-from
+                     (format "The required JS dependency \"%s\" is not available, it was required by \"%s\"." require (:resource-name require-from))
+                     (format "The required JS dependency \"%s\" is not available." require))
+                   {:tag ::missing-js
+                    :require require
+                    :require-from (:resource-name require-from)})))
+
+        (-> state
+            (data/maybe-add-source rc)
+            (cond->
+              require-from-ns
+              (data/add-string-lookup require-from-ns require ns)
+              (nil? require-from)
+              (update :js-entries conj ns))
+            (resolve-deps rc)
+            )))))
 
 (defn ensure-non-circular!
   [{:keys [resolved-stack] :as state} resource-id]

commit 6674f5afce106062001cb504f81bb083acd83689

fix JS reloading, accidentally broken in 0a3a66042b82ce6a1c98d6377792f749d81c9a7d

src/main/shadow/build/resolve.clj
@@ -219,7 +219,8 @@
   ;; FIXME: properly cleanup :str->sym, should not be possible to get some ns here but nil for rc
   (let [rc
         (when-let [resolved-sym (get-in state [:str->sym require-from-ns require])]
-          (data/get-source-by-provide state resolved-sym))]
+          (when-let [resource-id (get-in state [:sym->id resolved-sym])]
+            (get-in state [:sources resource-id])))]
     (if rc
       (resolve-deps state rc)
 