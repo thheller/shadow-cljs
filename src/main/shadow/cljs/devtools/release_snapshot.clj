fb67013b7af770d29c2e851c88f0e3010240d40b
Thomas Heller <th.heller@gmail.com>
2018-04-23 17:55:42 +0200

    tweak release-snapshot table

--- src/main/shadow/cljs/devtools/release_snapshot.clj
+++ src/main/shadow/cljs/devtools/release_snapshot.clj
@@ -15,43 +15,34 @@
   (pprint/print-table ks rows)
   (print "\u001B[m"))
 
-(defn short-resource-name [resource-name]
-  (cond
-    (str/includes? resource-name "node_modules")
-    (second (str/split resource-name #"/"))
-    :else resource-name))
-
 (defn get-optimized-info [build-modules]
   (->> build-modules
-       (map (fn [module]
-              (->> (:source-bytes module)
-                   (map (fn [[module-name bytes]] [module-name {:optimized-size bytes}]))
-                   (into {}))))
+       (map :source-bytes)
        (reduce (partial merge-with +))))
 
-(defn get-unoptimized-info [build-sources]
-  (reduce
-    (fn [m x]
-      (assoc m (:resource-name x) (select-keys x [:js-size :source-size])))
-    {}
-    build-sources))
-
 (defn get-bundle-summary [{:keys [build-modules build-sources] :as bundle-info}]
-  (let [optimized (get-optimized-info build-modules)
-        raw (get-unoptimized-info build-sources)
-        merged (build/deep-merge optimized raw)]
-    (->> merged
-         (group-by (fn [[k _]] (short-resource-name k)))
-         (map (fn [[k v]]
-                (let [resource-totals (->> v (map second) (reduce (partial merge-with +)))]
-                  (assoc resource-totals :short-resource-name k)))))))
+  (let [optimized (get-optimized-info build-modules)]
+    (->> build-sources
+         (map (fn [{:keys [package-name resource-name js-size source-size]}]
+                {:group (or package-name resource-name)
+                 :js-size js-size
+                 :source-size source-size
+                 :optimized-size (get optimized resource-name 0)}))
+         (group-by :group)
+         (map (fn [[group items]]
+                (->> items
+                     (map #(dissoc % :group))
+                     (reduce (partial merge-with +) {:group group}))))
+         (into [])
+         )))
 
 (defn print-bundle-info-table [bundle-info]
   (->> (get-bundle-summary bundle-info)
-       (sort-by #(or (:optimized-size %) 0) >)
-       (map (fn [{:keys [short-resource-name source-size js-size optimized-size]}]
-              {"Resource name" short-resource-name
-               "Optimized size" (some-> optimized-size (format-byte-size))
-               "Source size" (some-> source-size (format-byte-size))
-               "JS size" (some-> js-size (format-byte-size))}))
-       (print-table ["Resource name" "Optimized size" "JS size" "Source size"])))
+       (remove #(zero? (:optimized-size %)))
+       (sort-by :optimized-size >)
+       (map (fn [{:keys [group source-size js-size optimized-size]}]
+              {"Source" group
+               "Optimized size" (when (pos? optimized-size) (format-byte-size optimized-size))
+               "Source size" (format-byte-size source-size)
+               "JS size" (format-byte-size js-size)}))
+       (print-table ["Source" "Optimized size" "JS size" "Source size"])))
