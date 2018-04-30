d7b2b26488df235c518038a986698f03f6207539
Thomas Heller <th.heller@gmail.com>
2018-04-30 12:33:49 +0200

    didn't mean to commit this. breaks on CI.

--- src/dev/demo/worker.cljs
+++ src/dev/demo/worker.cljs
@@ -1,7 +1,6 @@
-(ns demo.worker
-  (:require ["react" :as r]))
+(ns demo.worker)
 
 ;; this ns in only executed by the worker
 
-(js/console.log "worker started" (r/createElement "h1" nil "hello world"))
+(js/console.log "worker started")
 (js/postMessage (pr-str (assoc {:x 1} :y 2)))
