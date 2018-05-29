8c3c025b3f55147b0972f405a38c058ebdc59154
Thomas Heller <th.heller@gmail.com>
2018-05-29 10:20:35 +0200

    add L externs just in case someone uses leaflet
    leaflet always exports window.L which can cause problems in
    :advanced builds.

--- src/main/shadow/cljs/externs/node.js
+++ src/main/shadow/cljs/externs/node.js
@@ -43,3 +43,13 @@ var shadow$loader = {};
  */
 var Java = {};
 Java.type = function(name) {};
+
+/**
+ * leaflet.js will always create a window.L global
+ * this can lead to very hard to debug errors in :advanced builds
+ * https://github.com/Leaflet/Leaflet/pull/2943
+ *
+ * just reserving it always doesn't harm builds that don't use it
+ * @const
+ */
+var L = {};
