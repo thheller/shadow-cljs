cb833fd344801fbfbcdb24772940007ef4cb8aed
Thomas Heller <th.heller@gmail.com>
2018-08-04 17:36:07 +0200

    bump 2.4.28
    somehow the npm 2.4.27 package ended up with the old code not
    actually including the AOT change.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "2.4.27"
+(defproject thheller/shadow-cljs "2.4.28"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
