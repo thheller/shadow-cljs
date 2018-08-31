1e8e187b824b81cafd4276c94c27c1d1c2eb7299
Thomas Heller <th.heller@gmail.com>
2018-08-31 18:20:20 +0200

    bump 2.6.6
    2.6.5 accidentally included a development version of the JS used
    for build reports and thus breaking them

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "2.6.5"
+(defproject thheller/shadow-cljs "2.6.6"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
