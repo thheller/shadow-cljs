5986e7953cbcc39c48ff7a74f01086082c3177b1
Thomas Heller <th.heller@gmail.com>
2018-11-30 14:50:04 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -26,13 +26,13 @@
 
    [org.clojure/data.json "0.2.6"]
    [org.clojure/tools.cli "0.3.7"]
-   [org.clojure/tools.reader "1.3.0"]
+   [org.clojure/tools.reader "1.3.2"]
    [nrepl "0.4.5"]
 
    [com.cognitect/transit-clj "0.8.313"]
    [com.cognitect/transit-cljs "0.8.256"]
 
-   [org.clojure/core.async "0.4.474"]
+   [org.clojure/core.async "0.4.490"]
 
    ;; hack to get the latest closure-compiler if CLJS doesn't have it
    [org.clojure/clojurescript "1.10.439"
@@ -41,21 +41,21 @@
 
    ;; [com.google.javascript/closure-compiler-unshaded "v20180319"]
    ;;  v20180506
-   [com.google.javascript/closure-compiler-unshaded "v20180910"]
+   [com.google.javascript/closure-compiler-unshaded "v20181125"]
 
    [thheller/shadow-util "0.7.0"]
    [thheller/shadow-client "1.3.2"]
 
-   [io.undertow/undertow-core "2.0.13.Final"]
+   [io.undertow/undertow-core "2.0.16.Final"]
 
    [hiccup "1.0.5"]
-   [ring/ring-core "1.7.0"
+   [ring/ring-core "1.7.1"
     :exclusions
     ;; used by cookie middleware which we don't use
     [clj-time]]
 
    [expound "0.7.1"]
-   [fipp "0.6.13"]
+   [fipp "0.6.14"]
 
    [com.wsscode/pathom "2.1.3"
     :exclusions
@@ -110,7 +110,7 @@
    :cljs
    {:java-opts ^:replace ["-XX:-OmitStackTraceInFastThrow"]
     :dependencies
-    [[fulcrologic/fulcro "2.6.7"
+    [[fulcrologic/fulcro "2.6.16"
       :exclusions
       [clojure-future-spec
        com.stuartsierra/component
@@ -120,7 +120,7 @@
      ;; just so the CI build has this downloaded
      ;; and cached before compiling the test-project
      [reagent "0.8.1"]
-     [nubank/workspaces "1.0.0-preview8"]]
+     [nubank/workspaces "1.0.1"]]
     :repl-options
     {:init-ns shadow.user
      :nrepl-middleware
