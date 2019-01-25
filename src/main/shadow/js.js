861d5e32e0272c727ad9503946b76a5cf1b52ddf
Thomas Heller <th.heller@gmail.com>
2019-01-25 22:01:10 +0100

    add warning when trying dynamic require at runtime
    makes a debugging of issues caused by this a bit easier. so far
    the only known case is "grommet".

--- src/main/shadow/js.js
+++ src/main/shadow/js.js
@@ -21,24 +21,24 @@ goog.define("shadow.js.NODE_ENV", "development");
  */
 goog.define("shadow.js.process.browser", false);
 
-
 /**
  * @dict
  */
 shadow.js.shims = {};
 
-
 // https://github.com/defunctzombie/node-process/blob/master/browser.js
 
 shadow.js.process["title"] = "browser";
 shadow.js.process["argv"] = [];
-shadow.js.process["cwd"] = function() { return '/'; };
+shadow.js.process["cwd"] = function() {
+  return "/";
+};
 
-shadow.js.process["version"] = ''; // empty string to avoid regexp issues
+shadow.js.process["version"] = ""; // empty string to avoid regexp issues
 shadow.js.process["versions"] = {};
-shadow.js.process["env"] = {"NODE_ENV":shadow.js.NODE_ENV};
+shadow.js.process["env"] = { NODE_ENV: shadow.js.NODE_ENV };
 
-shadow.js.process_noop = function() {}
+shadow.js.process_noop = function() {};
 
 shadow.js.process["on"] = shadow.js.process_noop;
 shadow.js.process["addListener"] = shadow.js.process_noop;
@@ -50,52 +50,86 @@ shadow.js.process["emit"] = shadow.js.process_noop;
 shadow.js.process["prependListener"] = shadow.js.process_noop;
 shadow.js.process["prependOnceListener"] = shadow.js.process_noop;
 
-shadow.js.process["listeners"] = function (name) { return [] }
-
-shadow.js.process["binding"] = function (name) {
-    throw new Error('process.binding is not supported');
+shadow.js.process["listeners"] = function(name) {
+  return [];
 };
 
-shadow.js.process["cwd"] = function () { return '/' };
-shadow.js.process["chdir"] = function (dir) {
-    throw new Error('process.chdir is not supported');
+shadow.js.process["binding"] = function(name) {
+  throw new Error("process.binding is not supported");
 };
-shadow.js.process["umask"] = function() { return 0; };
+
+shadow.js.process["cwd"] = function() {
+  return "/";
+};
+shadow.js.process["chdir"] = function(dir) {
+  throw new Error("process.chdir is not supported");
+};
+shadow.js.process["umask"] = function() {
+  return 0;
+};
+
+shadow.js.requireStack = [];
 
 /**
  * @return {ShadowJS}
  */
 shadow.js.jsRequire = function(name, opts) {
-  var module = shadow.js.files[name];
-
-  // module must be created before calling moduleFn due to circular deps
-  if (module === undefined) {
-    module = shadow.js.files[name] = {
-      "exports": {}
-    };
-  }
-
-  var moduleFn = shadow$provide[name];
-  if (moduleFn) {
-    delete shadow$provide[name];
-
-    var process = goog.global.process || shadow.js.process;
-
-    try {
-      moduleFn.call(module, goog.global, process, shadow.js.jsRequire, module, module["exports"], shadow.js.shims);
-    } catch (e) {
-      console.warn("shadow-cljs - failed to load", name);
-      throw e;
+  try {
+    if (goog.DEBUG) {
+      if (name.indexOf("/") != -1) {
+        console.warn(
+          "Tried to dynamically require '" +
+            name +
+            "' from '" +
+            shadow.js.requireStack[shadow.js.requireStack.length - 1] +
+            "'. This is not supported and may cause issues."
+        );
+      }
     }
 
-    if (opts) {
-      var globals = opts["globals"];
-      if (globals) {
-        for (var i = 0; i < globals.length; i++) {
-          window[globals[i]] = module["exports"];
+    shadow.js.requireStack.push(name);
+
+    var module = shadow.js.files[name];
+
+    // module must be created before calling moduleFn due to circular deps
+    if (module === undefined) {
+      module = shadow.js.files[name] = {
+        exports: {}
+      };
+    }
+
+    var moduleFn = shadow$provide[name];
+    if (moduleFn) {
+      delete shadow$provide[name];
+
+      var process = goog.global.process || shadow.js.process;
+
+      try {
+        moduleFn.call(
+          module,
+          goog.global,
+          process,
+          shadow.js.jsRequire,
+          module,
+          module["exports"],
+          shadow.js.shims
+        );
+      } catch (e) {
+        console.warn("shadow-cljs - failed to load", name);
+        throw e;
+      }
+
+      if (opts) {
+        var globals = opts["globals"];
+        if (globals) {
+          for (var i = 0; i < globals.length; i++) {
+            window[globals[i]] = module["exports"];
+          }
         }
       }
     }
+  } finally {
+    shadow.js.requireStack.pop();
   }
 
   return module["exports"];
@@ -105,7 +139,6 @@ shadow.js.jsRequire = function(name, opts) {
  * @dict
  */
 shadow.js.modules = {};
-
 
 shadow.js.require = function(name, opts) {
   return shadow.js.jsRequire(name, opts);
