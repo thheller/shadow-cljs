741d0923bbbdd939bd13b2be32241ab5e2ca0ed1
Thomas Heller <th.heller@gmail.com>
2019-06-05 20:50:08 +0200

    bump 2.8.39 - fix bad .38 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "2.8.38"
+(defproject thheller/shadow-cljs "2.8.39"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
