3f98c42c69e239952f5c68824664c30c0b638f47
Jimmy Miller <jimmyhmiller@gmail.com>
2019-06-12 04:53:40 -0400

    Allows bootstrapping to work in web-workers (#506)

--- src/main/shadow/cljs/bootstrap/browser.cljs
+++ src/main/shadow/cljs/bootstrap/browser.cljs
@@ -34,13 +34,8 @@
             (callback data)
             ))))))
 
-(defn script-eval
-  "js/eval doesn't get optimized properly, this hack seems to do the trick"
-  [code]
-  (let [node (js/document.createElement "script")]
-    (.appendChild node (js/document.createTextNode code))
-    (js/document.body.appendChild node)
-    (js/document.body.removeChild node)))
+(defn script-eval [code]
+  (js/goog.globalEval code))
 
 (defn execute-load! [compile-state-ref {:keys [type text uri ns provides] :as load-info}]
   #_ (js/console.log "load" type ns load-info)
