f2b93790c88a7bedac343d508ea71cedcb5bcae8
Thomas Heller <th.heller@gmail.com>
2019-06-29 11:33:02 +0200

    oops, actually fix it

--- test-project/src/main/test/es6.js
+++ test-project/src/main/test/es6.js
@@ -1,5 +1,4 @@
 import cljs from "goog:cljs.core";
-console.log("cjs exports", commonjs, cstar);
 export let foo = "es6/foo";
 export let map = cljs.assoc(null, "a", 1);
 export default "es6-default";
