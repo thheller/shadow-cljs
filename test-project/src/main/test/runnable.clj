56389d94c034ff6aadd54f69f92e44dd980e5edd
Thomas Heller <th.heller@gmail.com>
2019-09-26 15:39:43 +0200

    add quick hack to make CI fail with bad targets
    since there are no test builds for some targets errors may get
    lost. the namespaces should at least load properly, no clue
    how to validate builds properly though.

--- test-project/src/main/test/runnable.clj
+++ test-project/src/main/test/runnable.clj
@@ -1,4 +1,18 @@
-(ns test.runnable)
+(ns test.runnable
+  (:require
+    ;; FIXME: there should be proper test builds for each or these
+    ;; for now just testing if the targets actually load properly
+    [shadow.build.targets.azure-app]
+    [shadow.build.targets.bootstrap]
+    [shadow.build.targets.browser]
+    [shadow.build.targets.browser-test]
+    [shadow.build.targets.chrome-extension]
+    [shadow.build.targets.karma]
+    [shadow.build.targets.node-library]
+    [shadow.build.targets.node-script]
+    [shadow.build.targets.node-test]
+    [shadow.build.targets.npm-module]
+    [shadow.build.targets.react-native]))
 
 (defn foo []
   (prn :foo))
