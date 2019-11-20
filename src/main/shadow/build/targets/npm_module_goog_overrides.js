bd2bf1935b1714b05a63744c0c998d44afd6c1ed
Thomas Heller <th.heller@gmail.com>
2019-11-20 22:22:03 +0100

    remove goog.define override for :npm-module

--- src/main/shadow/build/targets/npm_module_goog_overrides.js
+++ src/main/shadow/build/targets/npm_module_goog_overrides.js
@@ -17,15 +17,3 @@ goog.provide = function(name) {
 goog.require = function(name) {
   return goog.getObjectByName(name, $CLJS);
 };
-
-// goog.define exports to global by default
-// we need it on CLJS_ENV
-goog.define = function(name, defaultValue) {
-  var value = defaultValue;
-
-  if (Object.prototype.hasOwnProperty.call(CLOSURE_DEFINES, name)) {
-    value = CLOSURE_DEFINES[name];
-  }
-
-  originalGoogExportPath(name, value, $CLJS);
-};
