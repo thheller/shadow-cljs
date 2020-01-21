061826de78f9e8bc936de5dcf599816c03806575
Thomas Heller <th.heller@gmail.com>
2020-01-21 12:19:16 +0100

    more cleanup

--- src/main/shadow/cljs/ui/components/builds.cljs
+++ src/main/shadow/cljs/ui/components/builds.cljs
@@ -1,1 +1,63 @@
-(ns shadow.cljs.ui.components.builds)
+(ns shadow.cljs.ui.components.builds
+  (:require
+    [shadow.experiments.grove :as sg :refer (<< defc)]
+    [shadow.cljs.ui.components.build-status :as build-status]
+    [shadow.cljs.model :as m]
+    ))
+
+
+(defc ui-builds-page []
+  [{::m/keys [builds]}
+   (sg/query-root
+     [{::m/builds
+       [::m/build-id]}])]
+  (<< [:div.flex-1.overflow-auto
+       [:div.p-2
+        (sg/render-seq builds ::m/build-id
+          (fn [{::m/keys [build-id] :as item}]
+            (<< [:div.py-1
+                 [:a.font-bold {:href (str "/build/" (name build-id))} (name build-id)]])))
+        ]]))
+
+(defc ui-build-page [build-ident]
+  [data
+   (sg/query-ident build-ident
+     [:db/ident
+      ::m/build-id
+      ::m/build-target
+      ::m/build-worker-active
+      ::m/build-status])
+
+   ::m/build-watch-compile! sg/tx
+   ::m/build-watch-start! sg/tx
+   ::m/build-watch-stop! sg/tx
+   ::m/build-compile! sg/tx
+   ::m/build-release! sg/tx]
+
+  (let [{::m/keys [build-id build-target build-status build-worker-active]} data]
+    (<< [:div.flex-1.overflow-auto
+         [:h1.text-xl.px-2.py-4 (name build-id) " - " (name build-target)]
+         [:div
+          [:div.p-2.text-lg.font-bold "Actions"]
+          [:div.p-2
+           (if build-worker-active
+             (<< [:button.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
+                  {:on-click [::m/build-watch-compile! build-id]}
+                  "force compile"]
+               [:button.ml-2.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
+                {:on-click [::m/build-watch-stop! build-id]}
+                "stop watch"])
+
+             (<< [:button.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
+                  {:on-click [::m/build-watch-start! build-id]}
+                  "start watch"]
+               [:button.ml-2.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
+                {:on-click [::m/build-compile! build-id]}
+                "compile"]
+               [:button.ml-2.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
+                {:on-click [::m/build-release! build-id]}
+                "release"]))]]
+
+         [:div.p-2
+          [:div.text-lg "Build Status"]
+          (build-status/render-build-status build-status)]])))
--- src/main/shadow/cljs/ui/main.cljs
+++ src/main/shadow/cljs/ui/main.cljs
@@ -1,77 +1,17 @@
 (ns shadow.cljs.ui.main
   (:require
-    [fipp.edn :refer (pprint)]
-    [shadow.experiments.arborist :as sa]
     [shadow.experiments.grove :as sg :refer (<< defc)]
+    [shadow.experiments.grove.main.loadable :refer (refer-lazy)]
     [shadow.experiments.grove.history :as history]
     [shadow.experiments.grove.worker-engine :as worker-eng]
-    [shadow.lazy :as lazy]
     [shadow.cljs.model :as m]
     [shadow.cljs.ui.components.inspect :as inspect]
-    [shadow.cljs.ui.components.build-status :as build-status]
     [shadow.cljs.ui.components.dashboard :as dashboard]
     [shadow.cljs.ui.components.runtimes :as runtimes]
+    [shadow.cljs.ui.components.builds :as builds]
     ))
 
-(defc ui-builds-page []
-  [{::m/keys [builds]}
-   (sg/query-root
-     [{::m/builds
-       [::m/build-id]}])]
-  (<< [:div.flex-1.overflow-auto
-       [:div.p-2
-        (sg/render-seq builds ::m/build-id
-          (fn [{::m/keys [build-id] :as item}]
-            (<< [:div.py-1
-                 [:a.font-bold {:href (str "/build/" (name build-id))} (name build-id)]])))
-        ]]))
-
-(defc ui-build-page [build-ident]
-  [data
-   (sg/query-ident build-ident
-     [:db/ident
-      ::m/build-id
-      ::m/build-target
-      ::m/build-worker-active
-      ::m/build-status])
-
-   ::m/build-watch-compile! sg/tx
-   ::m/build-watch-start! sg/tx
-   ::m/build-watch-stop! sg/tx
-   ::m/build-compile! sg/tx
-   ::m/build-release! sg/tx]
-
-  (let [{::m/keys [build-id build-target build-status build-worker-active]} data]
-    (<< [:div.flex-1.overflow-auto
-         [:h1.text-xl.px-2.py-4 (name build-id) " - " (name build-target)]
-         [:div
-          [:div.p-2.text-lg.font-bold "Actions"]
-          [:div.p-2
-           (if build-worker-active
-             (<< [:button.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
-                  {:on-click [::m/build-watch-compile! build-id]}
-                  "force compile"]
-                 [:button.ml-2.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
-                  {:on-click [::m/build-watch-stop! build-id]}
-                  "stop watch"])
-
-             (<< [:button.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
-                  {:on-click [::m/build-watch-start! build-id]}
-                  "start watch"]
-                 [:button.ml-2.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
-                  {:on-click [::m/build-compile! build-id]}
-                  "compile"]
-                 [:button.ml-2.py-2.px-4.bg-blue-200.hover:bg-blue-300.rounded.shadow
-                  {:on-click [::m/build-release! build-id]}
-                  "release"]))]]
-
-         [:div.p-2
-          [:div.text-lg "Build Status"]
-          (build-status/render-build-status build-status)]])))
-
-(def ui-repl-page
-  (sg/loadable-ui
-    (lazy/loadable shadow.cljs.ui.components.eval/ui-repl-page)))
+(refer-lazy shadow.cljs.ui.components.eval/ui-repl-page)
 
 (defc ui-root* []
   [{::m/keys [current-page]}
@@ -111,10 +51,10 @@
            (inspect/ui-page)
 
            :builds
-           (ui-builds-page)
+           (builds/ui-builds-page)
 
            :build
-           (ui-build-page (:ident current-page))
+           (builds/ui-build-page (:ident current-page))
 
            :dashboard
            (dashboard/ui-page)
