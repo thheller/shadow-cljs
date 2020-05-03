c2e5d937ab40637180fb610db8d2d361369d8cd8
Thomas Heller <th.heller@gmail.com>
2020-05-03 09:20:12 +0200

    [UI] fix svg path error

--- src/main/shadow/cljs/ui/components/builds.cljs
+++ src/main/shadow/cljs/ui/components/builds.cljs
@@ -58,7 +58,7 @@
 
 (def icon-build-missing
   (<< [:svg {:xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 24 24" :width "24" :height "24" :style "fill: rgba(0,0,0,0.1);"}
-       [:path {:d "M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2"}]]))
+       [:path {:d "M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0v-4a1 1 0 0 1 1-1zm0-4a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"}]]))
 
 (defc ui-builds-entry [build-ident]
   [{::m/keys [build-id build-worker-active build-warnings-count build-status] :as data}
