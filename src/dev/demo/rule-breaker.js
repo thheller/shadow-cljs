e92f891810e5e5a34c83bfa69e740332f29c0e16
Thomas Heller <th.heller@gmail.com>
2020-05-16 21:22:36 +0200

    add missing file

---
+++ src/dev/demo/rule-breaker.js
@@ -0,0 +1,6 @@
+// filename does not match exported name
+// lots of stuff in the closure-library doesn't either so this needs to be ok
+goog.module("demo.didnt_follow_the_rules");
+goog.module.declareLegacyNamespace();
+
+exports.foo = 1;
