d1990792afad4a8e5fa0d0cc96073e131e6bab48
Thomas Heller <th.heller@gmail.com>
2020-05-26 21:57:22 +0200

    bump cljs

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
    [org.clojure/core.async "1.2.603"]
 
    ;; hack to get the latest closure-compiler if CLJS doesn't have it
-   [org.clojure/clojurescript "1.10.758"
+   [org.clojure/clojurescript "1.10.773"
     :exclusions
     [com.google.javascript/closure-compiler-unshaded
      org.clojure/google-closure-library
