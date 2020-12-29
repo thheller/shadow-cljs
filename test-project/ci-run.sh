61ed64c86e77bbc28209232503460c60ec0a8bd9
Tom Parker-Shemilt <palfrey@tevp.net>
2020-12-29 21:09:22 +0000

    Add Karma to test projects

--- test-project/ci-run.sh
+++ test-project/ci-run.sh
@@ -8,6 +8,6 @@ npx shadow-cljs --cli-info
 
 npx shadow-cljs clj-run test.runnable/foo
 
-npx shadow-cljs release reagent test-node --verbose
+npx shadow-cljs release reagent test-node test-karma --verbose
 
 node out/test-node/script.js
