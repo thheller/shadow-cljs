9340b3c026cb7b35aa4202159168e3d9d3fe9404
Tom Parker-Shemilt <palfrey@tevp.net>
2021-01-27 23:21:53 +0000

    Use the circleci clojure image with node+browsers

--- test-project/ci-run.sh
+++ test-project/ci-run.sh
@@ -12,7 +12,4 @@ npx shadow-cljs release reagent test-node test-karma --verbose
 
 node out/test-node/script.js
 
-apt-get update
-apt-get install chromium --no-install-recommends -y
-
 CHROME_BIN=/usr/bin/chromium npx karma start --single-run
