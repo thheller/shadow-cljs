c7c831e19851ff4ef1fa4967abb60b469fc76b98
Thomas Heller <th.heller@gmail.com>
2021-02-04 21:57:14 +0100

    improve missing instance error

--- src/main/shadow/cljs/devtools/server/runtime.clj
+++ src/main/shadow/cljs/devtools/server/runtime.clj
@@ -8,7 +8,10 @@
 (defn get-instance! []
   (let [inst @instance-ref]
     (when-not inst
-      (throw (ex-info "missing instance" {})))
+      (throw (ex-info
+               (str "shadow-cljs has not been started yet!\n"
+                    "In embedded mode you need to call (shadow.cljs.devtools.server/start!) to start it.\n"
+                    "If you have a shadow-cljs server or watch running then you are not connected to that process.") {})))
     inst))
 
 (defn set-instance! [app]
