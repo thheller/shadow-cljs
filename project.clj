b62087bb482f845dd0304c90d9b4061d7bb1a211
Thomas Heller <th.heller@gmail.com>
2021-07-19 20:19:53 +0200

    bump 2.15.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "2.15.1"
+(defproject thheller/shadow-cljs "2.15.2"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
