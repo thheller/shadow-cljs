90b2969fc5f23cab4433922a7d405b308829a90b
Thomas Heller <th.heller@gmail.com>
2021-10-10 23:19:03 +0200

    bump 2.15.12, fix bad 2.15.11 release
    contained some leftover compiled code from a babel branch I
    experimented with creating some `babelHelpers` problems

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-cljs "2.15.11"
+(defproject thheller/shadow-cljs "2.15.12"
   :description "CLJS development tools"
   :url "https://github.com/thheller/shadow-cljs"
   :license {:name "Eclipse Public License"
