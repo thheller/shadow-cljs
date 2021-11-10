3eb99f1d368a441fa13075291199027e851fa17c
Thomas Heller <th.heller@gmail.com>
2021-11-10 17:58:06 +0100

    fix storybook load issue related to goog.module and :npm-module
    fixes #956

--- src/main/shadow/build/targets/npm_module_goog_overrides.js
+++ src/main/shadow/build/targets/npm_module_goog_overrides.js
@@ -14,6 +14,11 @@ goog.provide = function(name) {
   return originalGoogExportPath(name, undefined, undefined, $CLJS);
 };
 
+
+// in goog.module this needs to have a return value
+// the getObjectByName will only find modules that declareLegacyNamespace
+// otherwise get the module directly. can't use default goog.require since
+// we are never using the debug loader and it never has a return value in that case
 goog.require = function(name) {
-  return goog.getObjectByName(name, $CLJS);
+  return goog.getObjectByName(name, $CLJS) || goog.module.getInternal_(name);
 };
