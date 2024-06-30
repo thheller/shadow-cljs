9c8a8242ad092ebd46a789358c25d2b9456a9565
Thomas Heller <th.heller@gmail.com>
2024-06-30 08:02:26 +0200

    demo notify plugin

---
+++ src/dev/demo/notify_plugin.clj
@@ -0,0 +1,42 @@
+(ns demo.notify-plugin
+  (:require
+    [clojure.core.async :as async]
+    [shadow.jvm-log :as log]
+    [shadow.cljs :as-alias m]
+    [shadow.cljs.devtools.server.system-bus :as sys-bus]
+    ))
+
+(def plugin
+  {:depends-on [:system-bus]
+   :start
+   (fn [sys-bus]
+     (log/debug ::start)
+     (let [sub-chan (async/chan 512)]
+       (sys-bus/sub sys-bus ::m/worker-broadcast sub-chan)
+       {:chan sub-chan
+        :thread
+        (async/thread
+          (loop []
+            (when-some [msg (async/<!! sub-chan)]
+              ;; best use (tap> msg) to see exact structure of the msg data
+              (case (:type msg)
+                :build-start
+                (prn [:build-started (:build-id msg)])
+
+                :build-complete
+                (prn [:build-completed-successfully (:build-id msg)])
+
+                :build-failure
+                (prn [:build-failed (:build-id msg)])
+
+                ;; ignore others (currently only :build-configure, only emitted when build config changes)
+                nil)
+
+              (recur))))}))
+   :stop
+   (fn [{:keys [chan thread] :as svc}]
+     (log/debug ::stop)
+     (async/close! chan)
+     ;; wait for proper shutdown
+     (async/<!! thread)
+     ::done)})
